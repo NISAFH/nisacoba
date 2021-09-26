5220d975ff388cdf8d53db45a53ae443a3b1ba44
NISAFH <nisafauziyyah@student.telkomuniversity.ac.id>
2021-09-26 17:06:53 +0700

    HelloWorld.java

---
+++ Praktikum 2/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+public static void main (String[] args) {
+System.out.println ("Nama : Nisa Fauziyyah Handari");
+System.out.println ("Nim : 6706213138");
+System.out.println ("Kelas : D3 RPLA 45-05");
+
+}
+}
